bcee3ff0cf516fb9c0b7a1e4ef5caecafaba76ed
Heisenberg00 <abderrahim.zaghouri@gmail.com>
2021-10-12 09:08:02 +0100

    Using Aws CredentialProvider to connect ES (IAM)
    Using Aws CredentialProvider to connect ES with Identity Access Management (IAM)
    
    ADD CONSTANTS  IN YOUR .ENV FILE
    
    AWS_ACCESS_KEY_ID=...
    AWS_SECRET_ACCESS_KEY=....
    
    SCOUT_ELASTICSCOUT_AWS_ENABLED=...
    SCOUT_ELASTICSCOUT_AWS_REGION=..
    SCOUT_ELASTICSEARCH_HOST=https://.....eu-west-3.es.amazonaws.com
    SCOUT_ELASTICSEARCH_PORT=443
    SCOUT_ELASTICSEARCH_SCHEME=https
    
    AND ADD THIS CONFIG FILE IN YOUR CONFIG DIRECTORY:
    file: elasticscout.php
    
    
    return [
    
        'connection' => [
    
            /*
             * Define your Elasticsearch connection here.
             */
    
            'hosts' => [
                [
                    'host' => env('SCOUT_ELASTICSEARCH_HOST', '127.0.0.1'),
                    'port' => env('SCOUT_ELASTICSEARCH_PORT', 9200),
                    'scheme' => env('SCOUT_ELASTICSEARCH_SCHEME', null),
                    'user' => env('SCOUT_ELASTICSEARCH_USER', null),
                    'pass' => env('SCOUT_ELASTICSEARCH_PASSWORD', null),
    
                    'aws_enable' => env('SCOUT_ELASTICSCOUT_AWS_ENABLED', false),
                    'aws_region' => env('SCOUT_ELASTICSCOUT_AWS_REGION', 'us-east-1'),
                    'aws_key' => env('AWS_ACCESS_KEY_ID', ''),
                    'aws_secret' => env('AWS_SECRET_ACCESS_KEY', ''),
                ],
            ],
        ]
    ];

--- src/ElasticSearchServiceProvider.php
+++ src/ElasticSearchServiceProvider.php
@@ -8,12 +8,20 @@ use Elasticsearch\Client;
 use Elasticsearch\ClientBuilder;
 use Illuminate\Support\ServiceProvider;
 
+use Aws\ElasticsearchService\ElasticsearchPhpHandler;
+use Illuminate\Support\Facades\Config;
+use Aws\Credentials\CredentialProvider;
+use Aws\ElasticsearchService\ElasticsearchServiceClient;
+use InvalidArgumentException;
+use Laravel\Scout\EngineManager;
+
+
 final class ElasticSearchServiceProvider extends ServiceProvider
 {
     /**
      * {@inheritdoc}
      */
-    public function register(): void
+    public function register2(): void
     {
         $this->mergeConfigFrom(__DIR__.'/../config/elasticsearch.php', 'elasticsearch');
 
@@ -26,6 +34,35 @@ final class ElasticSearchServiceProvider extends ServiceProvider
             'Matchish\ScoutElasticSearch\ElasticSearch\EloquentHitsIteratorAggregate'
         );
     }
+    
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/elasticsearch.php', 'elasticsearch');
+        
+        $this->app->bind(Client::class, function () {
+
+            $aws_enabled = Config::get('elasticscout.connection.hosts.0.aws_enable');
+
+            $hosts = [config('elasticsearch.host')];
+            $client = ClientBuilder::create();
+
+            if ($aws_enabled) {
+                $hosts = [Config::get('elasticscout.connection.hosts.0.host') . ':' . Config::get('elasticscout.connection.hosts.0.port')];
+                $provider = CredentialProvider::defaultProvider();
+                $handler = new ElasticsearchPhpHandler('eu-west-3', $provider);
+                $client->setHandler($handler);
+            }
+            $client->setHosts($hosts);
+            return $client->build();
+        });
+
+        $this->app->bind(
+            'Matchish\ScoutElasticSearch\ElasticSearch\HitsIteratorAggregate',
+            'Matchish\ScoutElasticSearch\ElasticSearch\EloquentHitsIteratorAggregate'
+        );
+    }
+    
+    
 
     /**
      * {@inheritdoc}
